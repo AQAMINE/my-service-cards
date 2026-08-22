0dc0d4d674c38ae7d274d03c34d986834f67b708
aqamine <aqebliamine1@gmail.com>
2026-08-22 02:01:44 +0100

    Initial commit

---
+++ src/main/java/com/myservicecards/my_service_cards/MyServiceCardsApplication.java
@@ -0,0 +1,13 @@
+package com.myservicecards.my_service_cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyServiceCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyServiceCardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myservicecards/my_service_cards/MyServiceCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myservicecards.my_service_cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyServiceCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
